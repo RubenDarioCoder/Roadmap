ae9ee968cbdb1882ef5981c03224ee6eec62c3f5
RubenDarioCoder <dariomacielr@gmail.com>
2025-02-26 19:59:13 -0300

    cargar archivos

---
+++ Javascript/#00.js
@@ -0,0 +1,48 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+// Comentario de una línea
+/* Comentario de 
+varias 
+líneas */
+
+// Variables
+let variable = 'Hola, '; //Se puede modificar
+const constante = 'Javascript'; //No se puede modificar
+let string = 'Hola, mundo!';
+let number = 10;
+let numberdecimal = 10.5; 
+let bignumber = 9007199254740991n;
+let boolean = true;
+let boolean2 = false;
+let undefined;
+let nullvalue = null;
+let symbol = Symbol('Esto es in simbolo, unico e inmutable');
+console.log(variable + constante);
+
+
+
+
+
