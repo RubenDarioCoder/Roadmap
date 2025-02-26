bc1ecd8ee6bf17af11254b0ac508804468ab6ce2
RubenDarioCoder <dariomacielr@gmail.com>
2025-02-26 19:59:58 -0300

    Update #00.py

--- Python/#00.py
+++ Python/#00.py
@@ -1,13 +1,4 @@
 """
- * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
- * - Recuerda que todas las instrucciones de participación están en el
- *   repositorio de GitHub.
- *
- * Lo primero... ¿Ya has elegido un lenguaje?
- * - No todos son iguales, pero sus fundamentos suelen ser comunes.
- * - Este primer reto te servirá para familiarizarte con la forma de participar
- *   enviando tus propias soluciones.
- *
  * EJERCICIO:
  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
  *   lenguaje de programación que has seleccionado.
